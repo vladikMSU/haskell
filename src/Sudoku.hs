bf5fbf2814d3586f8e8dccbc97ed764fc1539ae0
Vladislav <vladik.ktl@gmail.com>
2017-10-08 02:59:31 +0300

    Update Sudoku.hs

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -6,19 +6,6 @@ import Graphics.Gloss.Juicy
 import Graphics.Gloss.Interface.Pure.Game
 
 
-data Image = Image {pic :: Picture, val :: Int}
-
-type Images = [Image]
-
-data Sudoku = Sudoku { rows :: [[Maybe Int]] }
-    deriving (Show)
-data Window = Window
-    { sudokuTable      :: Sudoku
-    , someCellSelected :: Bool
-    , currentCell      :: (Int, Int)
-    , images           :: Images
-    }
-
 windowWidth :: Int
 windowWidth = 800
 
@@ -37,60 +24,34 @@ borderThickness = 3
 tableSide::Float
 tableSide = 549
 
-getIthImage :: Maybe Picture -> Int -> Image
-getIthImage (Just pict) i = Image {pic = scale 0.05 0.05 pict, val = i}
 
-loadIms :: [Image] -> Int -> Images
-loadIms lst 9 = lst
-loadIms lst i = lst ++ (getIthImage ithPicture i)
-                where ithPicture = loadJuicyJPG ("images/" ++ [intToDigit i] ++ ".jpg")
+data Image = Image {pic :: Picture, val :: Int}
+
+type Images = [Image]
+
+data Sudoku = Sudoku { rows :: [[Maybe Int]] }
+    deriving (Show)
+
+data Window = Window
+    { sudokuTable      :: Sudoku
+    , someCellSelected :: Bool
+    , currentCellIdx   :: (Int, Int)
+    , images           :: Images
+    }
+
+getIthImage :: Picture -> Int -> Image
+getIthImage pict i = Image {pic = scale 0.05 0.05 pict, val = i}
+
+loadIms :: Int -> IO Images
+loadIms i | i > 9     = do
+                return []
+          | otherwise = do
+                theRest         <- loadIms (i+1)
+                Just ithPicture <- loadJuicyJPG ("images/" ++ [intToDigit i] ++ ".jpg")
+                return ((getIthImage ithPicture i) : theRest)
 
 loadImages :: IO Images
-loadImages = loadIms [] 0
-
-{-
-    Just imOne   <- loadJuicyJPG "images/1.jpg"
-    Just imTwo   <- loadJuicyJPG "images/2.jpg"
-    Just imThree <- loadJuicyJPG "images/3.jpg"
-    Just imFour  <- loadJuicyJPG "images/4.jpg"
-    Just imFive  <- loadJuicyJPG "images/5.jpg"
-    Just imSix   <- loadJuicyJPG "images/6.jpg"
-    Just imSeven <- loadJuicyJPG "images/7.jpg"
-    Just imEight <- loadJuicyJPG "images/8.jpg"
-    Just imNine  <- loadJuicyJPG "images/9.jpg"
-    return Images
-        { imageOne   = 
-        , imageTwo   = Image {pic = scale 0.05 0.05 imTwo,   val = 2}
-        , imageThree = Image {pic = scale 0.05 0.05 imThree, val = 3}
-        , imageFour  = Image {pic = scale 0.05 0.05 imFour,  val = 4}
-        , imageFive  = Image {pic = scale 0.05 0.05 imFive,  val = 5}
-        , imageSix   = Image {pic = scale 0.05 0.05 imSix,   val = 6}
-        , imageSeven = Image {pic = scale 0.05 0.05 imSeven, val = 7}
-        , imageEight = Image {pic = scale 0.05 0.05 imEight, val = 8}
-        , imageNine  = Image {pic = scale 0.05 0.05 imNine,  val = 9}
-        }
--}
-
-isValidSudoku :: Sudoku -> Bool
-isValidSudoku s = (length (rows s)) == 9 
-                  && and [length row == 9 | row <- (rows s)]
-                  && and [checkRow row    | row <- (rows s)]
-                  where checkRow row = and [isValidCell cell | cell <- row]
-                                       where isValidCell Nothing  = True
-                                             isValidCell (Just n) = n <= 9 && n >= 1
-                  
-parseSudoku :: String -> Sudoku
-parseSudoku str | not (isValidSudoku (parse str)) = error "parseSudoku: Not a sudoku."
-                | otherwise                       = parse str
-                where parse = Sudoku . (map (map symToCell)) . lines
-                              where symToCell '.' = Nothing
-                                    symToCell ch  = Just (digitToInt ch)
-
-readSudoku :: FilePath -> IO Sudoku
-readSudoku path = do
-    file_text <- readFile path
-    return (parseSudoku file_text)
-
+loadImages = (loadIms 1)
 
 generateWindow::FilePath -> IO Window
 generateWindow path = do
@@ -99,7 +60,7 @@ generateWindow path = do
     return Window
         { sudokuTable      = sudoku
         , someCellSelected = False
-        , currentCell      = (0, 0)
+        , currentCellIdx   = (0, 0)
         , images           = imgs
         }
 
@@ -130,29 +91,25 @@ generateBorders = pictures [ -- thick vertical borders
                            , translate 0 (-212) $ color black $ rectangleSolid tableSide 1
                            ]
 
-generateSudokuValsImages :: Window -> Picture
-generateSudokuValsImages win = pictures [ pic (imageOne (images win))
-                                        , translate 60 0  $ pic (imageFour (images win))
-                                        , translate 122 0 $ pic (imageNine (images win))]
-
-changeListElem :: [a] -> Int -> a -> [a]
-changeListElem lst i value | i == length lst = take (i-1) lst ++ [value]
-                           | otherwise       = take (i-1) lst ++ [value] ++ drop i lst
-
-changeSudokuElem :: Sudoku -> Int -> Int -> Int -> Sudoku
-changeSudokuElem s i j value = Sudoku (changeListElem (rows s) i changedRow)
-                               where changedRow = changeListElem oldRow j (Just value)
-                                                  where oldRow = head (drop (i-1) (rows s))
-
+--TODO
 indexToCoord :: (Int, Int) -> (Float, Float)
 indexToCoord (i,j) = (0,0)
 
 getBordersPicture :: (Float, Float) -> Picture
-getBordersPicture (x,y) = translate x y $ color red $ rectangleSolid 59 59
+getBordersPicture (x,y) = pictures [ translate x y $ color red   $ rectangleSolid 59 59
+                                   , translate x y $ color white $ rectangleSolid 49 49
+                                   ]
 
 genBordersForSelectedCell :: Window -> Picture
-genBordersForSelectedCell win | someCellSelected win = getBordersPicture (indexToCoord (currentCell win))                                 
+genBordersForSelectedCell win | someCellSelected win = getBordersPicture (indexToCoord (currentCellIdx win))                                 
                               | otherwise            = blank
+
+--TODO
+generateSudokuValsImages :: Window -> Picture
+generateSudokuValsImages win = pictures [blank]
+                                      {-(map (map f) (rows (sudoku win))) (imageOne (images win))
+                                        , translate 60 0  $ pic (imageFour (images win))
+                                        , translate 122 0 $ pic (imageNine (images win))]-}
 
 renderWindow :: Window -> Picture
 renderWindow window = pictures [ generateBorders
@@ -164,6 +121,7 @@ clickedOnTable :: (Float, Float) -> Bool
 clickedOnTable (x,y) = -tableSide/2 <= x && x <= tableSide/2
                     && -tableSide/2 <= y && y <= tableSide/2
 
+-- TODO
 coordToIndex :: (Float, Float) -> (Int, Int)
 coordToIndex (x,y) | -29 <= x && x <= 29 && -29 <= y && y <= 29 = (5,5)
                    | otherwise                                  = (1,1)
@@ -171,21 +129,38 @@ coordToIndex (x,y) | -29 <= x && x <= 29 && -29 <= y && y <= 29 = (5,5)
 processCellClicked :: Window -> (Float, Float) -> Window
 processCellClicked win click_coords
     | not (clickedOnTable click_coords) ||
-      someCellSelected win && coordToIndex click_coords == currentCell win 
+      someCellSelected win && coordToIndex click_coords == currentCellIdx win 
         = win { sudokuTable      = sudokuTable win
               , someCellSelected = False
               , images           = images win
-              , currentCell      = currentCell win
+              , currentCellIdx   = currentCellIdx win
               }
     | otherwise
         = win { sudokuTable      = sudokuTable win
               , someCellSelected = True
               , images           = images win
-              , currentCell      = coordToIndex click_coords
+              , currentCellIdx   = coordToIndex click_coords
               }
 
+changeListElem :: [a] -> Int -> a -> [a]
+changeListElem lst i value | i == length lst = take (i-1) lst ++ [value]
+                           | otherwise       = take (i-1) lst ++ [value] ++ drop i lst
+
+changeSudokuElem :: Sudoku -> (Int, Int) -> Int -> Sudoku
+changeSudokuElem s (i,j) value = Sudoku (changeListElem (rows s) i changedRow)
+                               where changedRow = changeListElem oldRow j (Just value)
+                                                  where oldRow = head (drop (i-1) (rows s))
+
 renewCurrentCellVal :: Int -> Window -> Window
-renewCurrentCellVal _ win = win
+renewCurrentCellVal value win
+    | not (someCellSelected win) = win
+    | otherwise                  = win
+                                   { sudokuTable      = changeSudokuElem (sudokuTable win) (currentCellIdx win) value
+                                   , someCellSelected = False
+                                   , currentCellIdx   = currentCellIdx win
+                                   , images           = images win
+                                   }
+
 
 handle :: Event -> Window -> Window
 handle (EventKey (MouseButton LeftButton) Down _ coordinates) window = processCellClicked window coordinates
@@ -215,3 +190,25 @@ start win = do
     where display = InWindow "Sudoku" (windowWidth, windowHeight) (windowOffsetLeft, windowOffsetTop)
           bgColor = white
           fps     = 60
+
+---------------------------------------------------------------------------------------
+
+isValidSudoku :: Sudoku -> Bool
+isValidSudoku s = (length (rows s)) == 9 
+                  && and [length row == 9 | row <- (rows s)]
+                  && and [checkRow row    | row <- (rows s)]
+                  where checkRow row = and [isValidCell cell | cell <- row]
+                                       where isValidCell Nothing  = True
+                                             isValidCell (Just n) = n <= 9 && n >= 1
+                  
+parseSudoku :: String -> Sudoku
+parseSudoku str | not (isValidSudoku (parse str)) = error "parseSudoku: Not a sudoku."
+                | otherwise                       = parse str
+                where parse = Sudoku . (map (map symToCell)) . lines
+                              where symToCell '.' = Nothing
+                                    symToCell ch  = Just (digitToInt ch)
+
+readSudoku :: FilePath -> IO Sudoku
+readSudoku path = do
+    file_text <- readFile path
+    return (parseSudoku file_text)
